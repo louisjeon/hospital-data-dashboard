79357e54c1d49bdc853e7850c9076fc873205d95
Louis Jeon <louisjeon@admins-MacBook-Pro.local>
2022-01-06 19:08:32 +0900

    fix HTTP error

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,16 @@
 import { Hero } from "../components/Hero";
 import { Container } from "../components/Container";
 import { PatientsTable } from "../components/PatientsTable";
+import Head from "next/head";
 
 const Index = () => (
   <Container>
+    <Head>
+      <meta
+        http-equiv="Content-Security-Policy"
+        content="upgrade-insecure-requests"
+      />
+    </Head>
     <Hero />
     <PatientsTable />
   </Container>
